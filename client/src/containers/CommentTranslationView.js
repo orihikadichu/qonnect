44b0324d47f364d6d497f9956e52e79ceca54a49
Akihiro Uchida <akihiro.uchida@world-scape.net>
2019-05-19 16:34:50 +0900

    [update] コメント翻訳ページのビューを整えた

--- client/src/containers/CommentTranslationView.js
+++ client/src/containers/CommentTranslationView.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ClipLoader } from 'react-spinners';
+import Linkify from 'react-linkify';
 import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
 import CommentTranslationForm from '../components/CommentTranslationForm';
 import CommentTranslationList from '../containers/CommentTranslationList';
 import {
@@ -41,13 +44,29 @@ class CommentTranslationView extends Component {
 
   render() {
     const { currentComment } = this.props.state.comments;
+    if (Object.keys(currentComment).length === 0) {
+      return (
+        <div className="uk-position-center uk-overlay uk-overlay-default">
+          <ClipLoader />
+        </div>
+      );
+    }
+
     const user = (currentComment.user) ? currentComment.user : null;
     const userName = (user) ? user.name : '';
     return (
       <main className="uk-container uk-container-small">
         <div className="uk-card uk-card-default uk-card-body uk-box-shadow-small">
-          <p>{currentComment.content}</p>
-          <p>{userName}</p>
+          <p><Linkify properties={{ target: '_blank'}} >{currentComment.content}</Linkify></p>
+          <p className="uk-text-meta">{dayjs(currentComment.created_at).format('YYYY/MM/DD HH:mm:ss')}</p>
+          <div className="uk-grid uk-grid-small uk-flex-middle" >
+            <div className="uk-width-auto">
+              <img className="uk-comment-avatar uk-border-circle" src={user.image_path} width="35" height="35" alt="" />
+            </div>
+            <div className="uk-width-expand">
+              <h4 className="uk-comment-meta uk-margin-remove"><Link className="" to={`/users/profile/${user.id}`}>{ user.name }</Link></h4>
+            </div>
+          </div>
         </div>
         <h3 className="uk-heading-line"><span>翻訳一覧</span></h3>
         <CommentTranslationForm commentId={this.commentId} onSubmit={this.handleSubmit.bind(this)} />
