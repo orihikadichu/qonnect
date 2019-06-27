69c3c17b7e6d7e482ee729ad2674b6517ec41d10
smizuka <satomizu2@hotmail.com>
2019-06-27 15:57:09 +0900

    いいねボタンの修正

--- client/src/components/PostIcons.js
+++ client/src/components/PostIcons.js
@@ -21,27 +21,23 @@ class PostIcons extends Component {
                         ? <Link className="uk-margin-small-right" to={editLink}><FontAwesomeIcon icon="edit" color="steelblue" size="lg"/></Link>
                         : '';
 
-    const editLinkButton = user.id === loginUser.id
-                         ? <Link className="uk-margin-small-right" to={editLink}><FontAwesomeIcon icon="edit" color="steelblue" size="lg"/></Link>
-                         : '';
+        const myVotes = votes.filter(v => {return v.user_id === loginUser.id});
+        const voteState = myVotes.length !== 0;
 
-    const myVotes = votes.filter(v => {return v.user_id === loginUser.id});
-    const voteState = myVotes.length !== 0;
-
-    let votebutton
-    if (voteState) {
-      votebutton = (
-        <a onClick={onClickDeleteVote.bind(this, deleteData, loginUser.id)}>
-          <FontAwesomeIcon icon="heart" color="red" size="lg"/>
-        </a>
-      );
-    } else {
-      votebutton = (
-        <a onClick={onClickSendVote.bind(this, sendData, loginUser.id)}>
-          <FontAwesomeIcon icon={['far','heart']} color="gray"  size="lg"/>
-        </a>
-      );
-    }
+        let votebutton
+        if (voteState) {
+        votebutton = (
+            <a onClick={onClickDeleteVote.bind(this, deleteData, loginUser.id)}>
+            <FontAwesomeIcon icon="heart" color="red" size="lg"/>
+            </a>
+        );
+        } else {
+        votebutton = (
+            <a onClick={onClickSendVote.bind(this, sendData, loginUser.id)}>
+            <FontAwesomeIcon icon={['far','heart']} color="gray"  size="lg"/>
+            </a>
+        );
+        }
 
         const translateButton = translate === true
                             ?<Link to={translateLink}><FontAwesomeIcon icon="globe-americas" color="steelblue" size="lg"/></Link>
