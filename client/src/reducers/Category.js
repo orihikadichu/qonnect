967d9be52fc047b67ddcb65647878bbd5f98cc93
Akihiro Uchida <akihiro.uchida@world-scape.net>
2019-07-05 13:03:36 +0900

    [update] カテゴリー、ソート関連処理の修正

--- client/src/reducers/Category.js
+++ client/src/reducers/Category.js
@@ -4,16 +4,15 @@ import { categories } from '../intl';
 
 export const ctgr = (state = initialState.ctgr, action) => {
 
-switch (action.type) {
-case UPDATE_CURRENT_CATEGORY:
+  switch (action.type) {
+  case UPDATE_CURRENT_CATEGORY:
     const { category, categoryId } = categories[action.payload];
-    localStorage.setItem('category', category);
     return {
-    ...state,
-    category,
-    categoryId
+      ...state,
+      category,
+      categoryId
     };
-default:
+  default:
     return state;
-}
+  }
 };
