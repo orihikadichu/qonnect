e90b62c6c6cd2ae0980bccce1b429fc4bb4e74e9
Akihiro Uchida <akihiro.uchida@world-scape.net>
2019-07-05 13:24:07 +0900

    deploy

---
+++ client/build/precache-manifest.0354ef01998bab8082ca3eb042d41a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "21d1846d859a3ca12ce9",
+    "url": "/static/css/main.8fb6cd8c.chunk.css"
+  },
+  {
+    "revision": "21d1846d859a3ca12ce9",
+    "url": "/static/js/main.21d1846d.chunk.js"
+  },
+  {
+    "revision": "16c7e6af6270b57ed81b",
+    "url": "/static/css/1.d26ae885.chunk.css"
+  },
+  {
+    "revision": "16c7e6af6270b57ed81b",
+    "url": "/static/js/1.16c7e6af.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5a82decbadd2a0b0b83392083a1d526",
+    "url": "/index.html"
+  }
+];
