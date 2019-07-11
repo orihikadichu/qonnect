766d64e491a5ec7ee2f3b590ee3764e52a3102e0
Akihiro Uchida <akihiro.uchida@world-scape.net>
2019-07-11 21:04:48 +0900

    deploy

---
+++ client/build/precache-manifest.f0aac9657c7f3541f4becf75a5ad512e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33a7fda37a73c4185c43",
+    "url": "/static/css/main.efb21ec1.chunk.css"
+  },
+  {
+    "revision": "33a7fda37a73c4185c43",
+    "url": "/static/js/main.33a7fda3.chunk.js"
+  },
+  {
+    "revision": "18e32bd7eef19b790237",
+    "url": "/static/css/1.d26ae885.chunk.css"
+  },
+  {
+    "revision": "18e32bd7eef19b790237",
+    "url": "/static/js/1.18e32bd7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96d9781c7932388a464f15cd4d60913f",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f0dcc869aa83a98df9e95f63b4b89ae6.js"
+  "/precache-manifest.f0aac9657c7f3541f4becf75a5ad512e.js"
 );
 
 workbox.clientsClaim();
