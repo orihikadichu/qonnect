b75deba5323383e2f8e5c38b8758c0fdd2469e67
Akihiro Uchida <akihiro.uchida@world-scape.net>
2019-07-18 15:12:19 +0900

    [update] リファクタ

--- client/src/containers/QuestionView.js
+++ client/src/containers/QuestionView.js
@@ -1,32 +1,28 @@
 import { connect } from 'react-redux';
 import QuestionView from '../components/QuestionView';
 import {
-    fetchQuestion
+  fetchQuestion
 } from '../actions/Question';
 import {
-    postAnswerData,
-    fetchAnswerList
+  postAnswerData,
+  fetchAnswerList
 } from '../actions/Answer';
 //評価するための関数
 import {
-    postVote,
-    deleteVote,
-    handleVote
+  handleVote
 } from '../actions/Vote';
 
 const mapStateToProps = state => {
-    return {state};
+  return {state};
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        getQuestionById: (id) => dispatch(fetchQuestion(id)),
-        handleSubmit: (postData) => dispatch(postAnswerData(postData)),
-        fetchAnswers: (question_id) => dispatch(fetchAnswerList(question_id)),
-        // handlePostVote: (data) => dispatch(postVote(data)),
-        // handleDeleteVote: (data) => dispatch(deleteVote(data)),
-        handleVote: (data) => dispatch(handleVote(data)), 
-    };
+  return {
+    getQuestionById: (id) => dispatch(fetchQuestion(id)),
+    handleSubmit: (postData) => dispatch(postAnswerData(postData)),
+    fetchAnswers: (question_id) => dispatch(fetchAnswerList(question_id)),
+    handleVote: (data) => dispatch(handleVote(data)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(QuestionView);
